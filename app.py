8670a8bb3ce16cd84f8e7f57e2031e9703e0791d
ranishinde9 <ranicshinde123@gmail.com>
2025-02-20 12:48:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 import numpy as np  # Ensure numpy is imported
 import pandas as pd
 from sklearn.metrics import accuracy_score
+import os
 
 st.set_page_config(page_title="Diabetes Prediction", layout="wide", page_icon="🧑‍⚕")
 
@@ -53,7 +54,7 @@ st.markdown(
 )
 
 # Load the model
-diabetes_model_path = r"C:\Users\RANI SHINDE\OneDrive\Desktop\workshop2\diabetes_model.sav"
+diabetes_model_path = "diabetes_model.sav"
 try:
     with open(diabetes_model_path, 'rb') as model_file:
         diabetes_model = pickle.load(model_file)
